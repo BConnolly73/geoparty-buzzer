4a203780b5ace15612e65509e4c804c8d81c376e
Bradley Connolly <bconnolly73@gmail.com>
2020-06-20 17:29:06 -0400

    Fix host

--- client/src/Connection/index.tsx
+++ client/src/Connection/index.tsx
@@ -6,7 +6,7 @@ import { Participant } from './../Types/participant';
 console.log(process.env.NODE_ENV);
 const socket = io(
     process.env.NODE_ENV === 'production' ?
-    'https://calm-fjord-53303.herokuapp.com/' :
+    'https://geoparty-trivia.herokuapp.com/' :
     'http://localhost:4001'
 );
 
